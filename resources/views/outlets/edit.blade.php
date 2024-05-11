14397e8738405920b147ccc4f12aae6b5f06c1df
Haris A <129580899+SapiKabur@users.noreply.github.com>
2024-05-11 18:06:48 +0700

    Update edit.blade.php

--- resources/views/outlets/edit.blade.php
+++ resources/views/outlets/edit.blade.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html>
 <head>
-    <title>Laravel 11 Import Export Excel to Database</title>
+    <title>PSAS Laundry</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" crossorigin="anonymous">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css" />
     <link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.css">
