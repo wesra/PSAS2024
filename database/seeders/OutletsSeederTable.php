c41ad93b66a40cb089d2c504bad33c80b642e1ae
Haris A <129580899+SapiKabur@users.noreply.github.com>
2024-05-11 17:50:09 +0700

    Update OutletsSeederTable.php

--- database/seeders/OutletsSeederTable.php
+++ database/seeders/OutletsSeederTable.php
@@ -15,44 +15,5 @@ class OutletsSeederTable extends Seeder
      */
     public function run(): void
     {
-        Outlet::create([
-            'code' => Str::random(),
-            'name' => 'Mahendra Haris',
-            'status' => 'selesai',
-            'address' => 'Cijujung Tengah',
-            'phone' => '081543195246',
-        ]);
-
-        Outlet::create([
-            'code' => Str::random(),
-            'name' => 'Gayu Ol',
-            'status' => 'Berlangsung',
-            'address' => 'GBK',
-            'phone' => '089087655633',
-        ]);
-
-        Outlet::create([
-            'code' => Str::random(),
-            'name' => 'Mala Arwita',
-            'status' => 'selesai',
-            'address' => 'Sydney',
-            'phone' => '081198673542',
-        ]);
-
-        Outlet::create([
-            'code' => Str::random(),
-            'name' => 'Haris Altam',
-            'status' => 'berlangsung',
-            'address' => 'pantai kuta',
-            'phone' => '0898734652097',
-        ]);
-
-        Outlet::create([
-            'code' => Str::random(),
-            'name' => 'Mala Arwita',
-            'status' => 'selesai',
-            'address' => 'Sydney',
-            'phone' => '087765367829',
-        ]);
     }
 }
